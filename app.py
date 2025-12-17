a572421bf75c6457a009f6bcc3204228440330a4
Deeran Venkatapathy <deeran@vmgdigital.com>
2025-12-17 11:54:52 +0530

    "relese__1.1.12"

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ class UpdateProgressDialog(QDialog):
 
 from updater_client import check_for_update
 
-APPVERSION = "1.1.11"  # your current version
+APPVERSION = "1.1.12"  # your current version
 
 
 from PySide6.QtGui import QIcon, QTextCursor, QAction, QCursor, QFont,QPixmap
